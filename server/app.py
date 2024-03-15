c87d50db14cc7cc5c82f72e3ebb9104bedbeafc2
Elise Erickson <elisece717@gmail.com>
2024-03-15 15:18:52 -0400

    simplifies returns using serialization

--- server/app.py
+++ server/app.py
@@ -20,6 +20,30 @@ def index():
     body = {'message': 'Welcome to the pet directory!'}
     return make_response(body, 200)
 
+@app.route('/pets/<int:id>')
+def pet_by_id(id):
+    pet = Pet.query.filter(Pet.id==id).first()
+
+    if pet:
+        body = pet.to_dict()
+        status = 200
+    else:
+        status = 400
+        body = {'message': f'Pet {id} not found.'}
+
+    return make_response(body, status)
+
+@app.route('/species/<string:species>')
+def pest_by_species(species):
+    pets = Pet.query.filter_by(species=species).all()
+    pets_json = []
+    for pet in pets:
+        pets_json.append(pet.to_dict())
+    
+    body = {"pets": pets_json, 'count': len(pets)}
+
+    return make_response(body, 200)
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
